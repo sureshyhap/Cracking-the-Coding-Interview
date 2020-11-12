190324eb2aa9836b6c4bcc9fe08699d1f06b8206
Suresh Yhap <sureshyhap@gmail.com>
2020-11-12 14:29:12 -0500

    Cleaned up 2.2

--- Chapter 2/2/two.cpp
+++ Chapter 2/2/two.cpp
@@ -5,11 +5,15 @@ template <typename T>
 T kth_to_last(typename std::forward_list<T>& lyst, typename std::forward_list<T>::iterator it, int k);
 
 int main(int argc, char* argv[]) {
+  std::cout << "Enter k: ";
+  int k = {};
+  std::cin >> k;
   std::forward_list<int> fl = {1, 5, 2, 56, 3, 6, 2, 4, 1, 4, 5};
-  std::cout << kth_to_last(fl, fl.begin(), 5);
+  std::cout << kth_to_last(fl, fl.begin(), k);
   return 0;
 }
 
+// Returns kth to last element of a singly linked list
 template <typename T>
 T kth_to_last(typename std::forward_list<T>& lyst, typename std::forward_list<T>::iterator it, int k) {
   static bool reached_end = {false}, found = {false};
@@ -21,15 +25,17 @@ T kth_to_last(typename std::forward_list<T>& lyst, typename std::forward_list<T>
   if (!reached_end) {
     kth_to_last(lyst, std::next(it), k);
   }
-  if (!found and place != -1) {
-    data = *it;
-  }
-  if (k == place++) {
-    found = true;
-    --place;
-    return data;
+  if (!found) {
+    if (k == place++) {
+      found = true;
+      data = {*it};
+      return data;
+    }
+    else {
+      return T();
+    }
   }
   else {
-    return T();
+    return data;
   }
 }
